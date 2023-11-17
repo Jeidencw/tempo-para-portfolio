c7e7e2d5ab0453e06b528f661a5c2e6f27c084ec
Jeidencw <henriquecw22@gmail.com>
2023-11-17 17:27:54 -0300

    inicio do weather js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const menuButtons = document.querySelectorAll('.menu button')
+const menuButtons = document.querySelectorAll('.btn__day')
+const menuIcons = document.querySelectorAll('.menu__icon')
+const infoOneEl = document.querySelector('.info__one-day')
+const infoFiveEl = document.querySelector('.info__five-days')
+
 
 menuButtons.forEach(button => {
     button.addEventListener('click', e => {
         menuButtons.forEach(btn => btn.classList.remove('active'))
+        menuIcons.forEach(icon => icon.classList.remove('active__icon'))
     
         e.target.classList.add('active')
+        e.target.nextElementSibling.classList.add('active__icon')
+
+        if(e.target.textContent === 'Hoje'){
+            infoOneEl.style.display = 'flex'
+            infoFiveEl.style.display = 'none'
+        }else{
+            infoOneEl.style.display = 'none'
+            infoFiveEl.style.display = 'flex'
+        }
     })
 })
--- weather.js
+++ weather.js
@@ -2,18 +2,44 @@ const cityEl = document.querySelector('.city')
 const dateEl = document.querySelector('.date')
 const temperatureEl = document.querySelector('.temperature')
 const min_maxEl = document.querySelector('.min-max')
+const inputSearchEl = document.querySelector('.input__search')
+const menuSearchEl = document.querySelector('.menu__search')
 
-const url = city => `https://api.hgbrasil.com/weather?key=ea6e58cc&city_name=${city}`
 
-const fetchData = async url => {
+const urlCity = city => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=BiAf1nsRQlViFAzg8IpzshCsAGzUSnA2&q=${city}`
+const urlCurrentConditions = cityKey => `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=BiAf1nsRQlViFAzg8IpzshCsAGzUSnA2&language=pt-br`
+const urlFiveDaysForecast = cityKey => `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=BiAf1nsRQlViFAzg8IpzshCsAGzUSnA2&language=pt-br&metric=true`
+
+const fetchDataCity = async url => {
     const response = await fetch(url)
-
-    const data = response.json()
-
-    console.log(data);
+    return await response.json()
 }
 
-fetchData(url('Curitiba'))
+const fetchCurrentConditions = async url => {
+    const response = await fetch(url)
+    return await response.json()
+}
+
+const fetchFiveDaysForecast = async url => {
+
+}
+
+
+menuSearchEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const inputValue = inputSearchEl.value.trim()
+
+    const dataCity = await fetchDataCity(urlCity(inputValue))
+    const dataCurrentConditions = await fetchCurrentConditions(urlCurrentConditions(dataCity[0].Key))
+
+    const cityName = dataCity[0].LocalizedName
+    const currentTemperature = dataCurrentConditions[0].Temperature.Metric.Value
+
+    cityEl.textContent = cityName
+    temperatureEl.textContent = currentTemperature
+
+    inputSearchEl.value = ''
+})
 
 
 // if ("geolocation" in navigator) {
